e84136a4a0f59e87ac0bae3fa1ac244ee7cec0e6
mos3c <elijahesan2@gmail.com>
2025-03-29 14:25:41 +0100

    Use whitenoise to serve static files on Vercel

--- mos3c/settings.py
+++ mos3c/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "mos3c/static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files for uploads
